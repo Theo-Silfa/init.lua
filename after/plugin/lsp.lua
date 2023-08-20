9af26c4e60a19e8fe923407cc9eaf415c2e338a5
Fedor.Lisochenko <fedor.lisochenko@gmail.com>
2023-08-20 16:30:23 +0200

    Initial commit

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,35 @@
+local lsp = require('lsp-zero').preset({})
+
+lsp.on_attach(function(client, bufnr)
+  lsp.default_keymaps({buffer = bufnr})
+
+  vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<cr>', {buffer = true})
+end)
+
+-- Fix Undefined global 'vim'
+lsp.nvim_workspace()
+
+lsp.ensure_installed({
+  'clangd',
+  'lua_ls'
+})
+
+lsp.preset({
+  manage_nvim_cmp = {
+    set_source = 'recommended', 
+  }
+})
+
+local cmp = require('cmp')
+
+cmp.setup({
+  mapping = {
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+  },
+  preselect = 'item',
+  completion = {
+    completeopt = 'menu,menuone,noinsert'
+  },
+})
+
+lsp.setup()
---
+++ lua/silfa/init.lua
@@ -0,0 +1,6 @@
+require("silfa.remap")
+require("silfa.packer")
+require("silfa.set")
+
+vim.cmd('language en_US')
+
