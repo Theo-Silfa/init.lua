e4ef8110281597faf2553796f5f616d33f9c1795
Fedor.Lisochenko <fedor.lisochenko@gmail.com>
2023-08-21 22:48:21 +0200

    Buffer navigation via telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,13 +4,14 @@ vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
+vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
 
 local actions = require("telescope.actions")
 require("telescope").setup({
     defaults = {
         mappings = {
             i = {
-                ["<esc>"] = actions.close,
+                ["<c-d>"] = actions.delete_buffer + actions.move_to_top,
             },
         },
     },
