b2a2ea0c304c8e771c0379bf029e228e243b3b1d
Fedor.Lisochenko <fedor.lisochenko@gmail.com>
2023-08-24 21:17:40 +0200

    More selected remaps

--- lua/silfa/remap.lua
+++ lua/silfa/remap.lua
@@ -1,8 +1,24 @@
 vim.g.mapleader = ' '
+
+-- open file EXplorer
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- move selected lines
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- half page jump and stay at the center
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+-- half page search and stay at the center
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- copy selection to system buffer
 vim.keymap.set({"n", "v"}, "<leader>y", "\"+y")
+-- copy line to system buffer
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+-- replace the current word
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
