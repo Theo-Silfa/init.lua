706a4ca86b47541abf2266aebbdd34be67b2a4dd
Fedor.Lisochenko <fedor.lisochenko@gmail.com>
2023-09-06 00:00:07 +0200

    Tab buffers setup and remaps

--- lua/silfa/packer.lua
+++ lua/silfa/packer.lua
@@ -1,8 +1,3 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
-
--- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
-
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
@@ -40,4 +35,7 @@ return require('packer').startup(function(use)
   use 'lewis6991/gitsigns.nvim'
 
   use 'ojroques/nvim-osc52'
+
+  use 'nvim-tree/nvim-web-devicons'
+  use 'romgrk/barbar.nvim'
 end)
